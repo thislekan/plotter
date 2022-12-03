1d46ae1916f47309ad3faee882348eaabc36ffbd
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2022-12-03 02:45:11 +0100

    - initial app setup [more like final]

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CanvasView } from "./components/canvas/canvas-view";
+import { ControlComponent } from "./components/controls/controls";
+import { StatsView } from "./components/stats/stats";
+import { useState } from "react";
+import "./index.scss";
 
 function App() {
+  const [imageData, setImageData] = useState("");
+  const [shapeCount, setShapeCount] = useState(0);
+  const [pointsCount, setPointsCount] = useState(0);
+  const [undoneSteps, setUndoneSteps] = useState(0);
+  const [exportCount, setExportCount] = useState(0);
+  const [view, setView] = useState(1);
+
+  const handleImageData = (data: string) => setImageData(data);
+  const toggleView = () => setView((view) => (view !== 1 ? 1 : 2));
+  const handleExportCount = (reset?: boolean) => {
+    setExportCount((count) => (reset ? 0 : count + 1));
+  };
+  const handleUndoneStepsCount = (reset?: boolean) => {
+    setUndoneSteps((step) => (reset ? 0 : step + 1));
+  };
+  const handleShapesCount = (reset?: boolean) => {
+    setShapeCount((count) => (reset ? 0 : count + 1));
+  };
+  const handlePointsCount = (reset?: boolean) => {
+    setPointsCount((count) => (reset ? 0 : count + 1));
+  };
+  const resetStats = () => {
+    handleExportCount(true);
+    handlePointsCount(true);
+    handleShapesCount(true);
+    handleUndoneStepsCount(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="tabs-container">
+        <button
+          className="tabs-container__btn tabs-container__btn--left"
+          onClick={toggleView}
+          disabled={view === 1}
         >
-          Learn React
-        </a>
-      </header>
+          Create Shapes
+        </button>
+        <button
+          className="tabs-container__btn tabs-container__btn--right"
+          onClick={toggleView}
+          disabled={view === 2}
+        >
+          View Stats
+        </button>
+      </div>
+      {view === 1 ? (
+        <>
+          <CanvasView
+            imageData={imageData}
+            handlePointsCount={handlePointsCount}
+            handleShapesCount={handleShapesCount}
+            handleUndoneStepsCount={handleUndoneStepsCount}
+            resetStats={resetStats}
+          />
+          <ControlComponent
+            storeFileFunc={handleImageData}
+            handleExportCount={handleExportCount}
+          />
+        </>
+      ) : (
+        <StatsView
+          shapeCount={shapeCount}
+          clickCount={pointsCount}
+          undoneSteps={undoneSteps}
+          exportCount={exportCount}
+        />
+      )}
     </div>
   );
 }
